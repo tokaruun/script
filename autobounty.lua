0d3193122a6370507d21477252e7d93e0f1b3950
tokaruun <tokaruunhi@gmail.com>
2025-11-18 14:26:21 +0700

    Update autobounty.lua

--- autobounty.lua
+++ autobounty.lua
@@ -1,4 +1,5 @@
 local placeId = game.PlaceId
+local LocalPlayer = game:GetService("Players").LocalPlayer
 
 if placeId == 85211729168715 or placeId == 2753915549 or placeId == 79091703265657 or placeId == 4442272183 or placeId == 100117331123089 or placeId == 7449423635 then 
 loadtring(game:HttpGet("https://raw.githubusercontent.com/tokaruun/script/refs/heads/main/loadbounty.lua")()
